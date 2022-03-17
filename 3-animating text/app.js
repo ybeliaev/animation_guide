957f0330aae0aad567637fe25f36ad6a6e564b0b
Yurij Beliaev <ybeliaev@gmail.com>
2022-03-17 14:19:36 +0200

    done animating text

--- 3-animating text/app.js
+++ 3-animating text/app.js
@@ -13,8 +13,24 @@ tl.fromTo(
   );
   tl.fromTo(".cta1",{ x: "-100%", opacity: 0 }, { x: 0, opacity: 1 }, "<20%")
   tl.fromTo(".cta2",{ y: "100%", opacity: 0 }, { y: 0, opacity: 1 }, "<20%")
-  tl.fromTo(".cta3",{ x: "-100%", opacity: 0 }, { x: 0, opacity: 1 }, "<20%")
+  tl.fromTo(".cta3",{ x: "100%", opacity: 0 }, { x: 0, opacity: 1 }, "<20%")
   tl.fromTo(".cta4",{ x: "-100%", opacity: 0 }, { x: 0, opacity: 1 }, "<20%")
   tl.fromTo(".cta5",{ y: "100%", opacity: 0 }, { y: 0, opacity: 1 }, "<20%")
   tl.fromTo(".cta6",{ x: "100%", opacity: 0 }, { x: 0, opacity: 1 }, "<20%")
   tl.fromTo(".cta-btn",{ y: "50%", opacity: 0 }, { y: 0, opacity: 1 }, "<20%")
+
+  // split header text
+  const logo = document.querySelector(".logo")
+  const letters = logo.textContent.split("")
+    logo.textContent=""
+
+  letters.forEach((letter)=>{
+    logo.innerHTML += `<span class="letter">${letter}</span>`
+  })
+  gsap.set(".letter", { display: "inline-block" });
+  gsap.fromTo(
+    ".letter",
+    { y: "100%", opacity: 0 },
+    { y: 0, opacity:1 ,delay: 2, stagger: 0.1, ease: "back.out(3)" }
+  );
+  
