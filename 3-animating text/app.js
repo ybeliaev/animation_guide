4508e01c0a115a5fedb6fe451b14c22b5cf926f5
Yurij Beliaev <ybeliaev@gmail.com>
2022-03-17 12:55:35 +0200

    gsap for bg and text

---
+++ 3-animating text/app.js
@@ -0,0 +1,20 @@
+const tl = gsap.timeline({default:{duration:0.75,ease: "Power3.easeOut"}})
+
+tl.fromTo(
+    ".hero-section",
+    { scale: 1.3, borderRadius: "0rem" },
+    {
+      scale: 1,
+      borderRadius: "2rem",
+      delay: 0.35,
+      duration: 2.5,
+      ease: "elastic.out(1.5,1)",
+    }
+  );
+  tl.fromTo(".cta1",{ x: "-100%", opacity: 0 }, { x: 0, opacity: 1 }, "<20%")
+  tl.fromTo(".cta2",{ y: "100%", opacity: 0 }, { y: 0, opacity: 1 }, "<20%")
+  tl.fromTo(".cta3",{ x: "-100%", opacity: 0 }, { x: 0, opacity: 1 }, "<20%")
+  tl.fromTo(".cta4",{ x: "-100%", opacity: 0 }, { x: 0, opacity: 1 }, "<20%")
+  tl.fromTo(".cta5",{ y: "100%", opacity: 0 }, { y: 0, opacity: 1 }, "<20%")
+  tl.fromTo(".cta6",{ x: "100%", opacity: 0 }, { x: 0, opacity: 1 }, "<20%")
+  tl.fromTo(".cta-btn",{ y: "50%", opacity: 0 }, { y: 0, opacity: 1 }, "<20%")
