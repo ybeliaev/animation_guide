299f28d78cf32b397743827d4fc319ca3b2b4020
Yurij Beliaev <ybeliaev@gmail.com>
2022-03-25 11:26:46 +0200

    button event animatiom done

--- 5 - form animation/app.js
+++ 5 - form animation/app.js
@@ -142,3 +142,44 @@ checkbox.addEventListener("click", () => {
         tl2.to(".checkbox-label", {color: "#c5c5c5"}, "<");
     }
 })
+//**************Animating Character
+// eye movement
+gsap.set("#eye", { transformOrigin: "center" });
+gsap.fromTo(
+    "#eye",
+    { scaleY: 1 },
+    {
+        scaleY: 0.3,
+        repeat: -1,
+        yoyo: true,
+        repeatDelay: 0.5,
+        ease: "Power2.easeOut",
+    }
+);
+gsap.fromTo(
+    "#eyebrow",
+    { y: 0 },
+    { y: -1, repeat: -1, yoyo: true, repeatDelay: 0.5, ease: "Power2.easeOut" }
+);
+//Submit button
+const button = document.querySelector("button");
+const tl3 = gsap.timeline({
+    defaults: { duration: 0.75, ease: "Power2.easeOut" },
+});
+button.addEventListener("click", (e) => {
+    e.preventDefault();
+    tl3.to(".contact-right, .contact-left", {
+        y: 30,
+        opacity: 0,
+        pointerEvents: "none",
+    });
+    tl3.to("form", { scale: 0.8 }, "<");
+    tl3.fromTo(".submitted", { opacity: 0, y: 30 }, { opacity: 1, y: 0 });
+    //Man Hand wave
+    gsap.set("#hand", { transformOrigin: "left" });
+    gsap.fromTo(
+        "#hand",
+        { rotation: 0, y: 0 },
+        { rotation: -10, y: 2, ease: "elastic(3,0.3)", duration: 2, delay: 1 }
+    );
+});
