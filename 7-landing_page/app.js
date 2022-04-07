51509b12cfbc412ba223d83418225a129d3d5a92
Yurij Beliaev <ybeliaev@gmail.com>
2022-04-07 18:47:47 +0300

    add two page highlighte

--- 7-landing_page/app.js
+++ 7-landing_page/app.js
@@ -8,3 +8,30 @@ const tlIntro = gsap.timeline({
         markers: true
     }
 })
+
+// TWO PAGE highlight
+const tlH = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".second-page",
+        markers: {startColor: "blue", endColor: "blue"},
+        scrub: 1,
+        start: "-40%",
+        end: "40%",
+    }
+})
+tlH.fromTo(
+    ".highlight",
+    { color: "rgba(255,255,255, 0.4" },
+    { color: "rgba(255,255,255, 1", stagger: 1 }
+);
+// возврат к первоначальному цвету
+const tlHRemove = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".second-page",
+        markers: { startColor: "pink", endColor: "pink" },
+        scrub: true,
+        start: "-20%",
+        end: "60%",
+    }
+})
+tlHRemove.to(".highlight", { color: "rgba(255,255,255, 0.4", stagger: 1 });
