a152aadd652988b88a375b64167c69ecf345c3b7
ybeliaev <ybeliaev@gmail.com>
2022-05-31 14:14:59 +0300

    add @media to css and js

--- 13-wakefast/main.js
+++ 13-wakefast/main.js
@@ -10,82 +10,82 @@ const figcaptionTags = document.querySelectorAll('figcaption')
 const cursorTag = document.querySelector("div.cursor")
 const cursorOuterTag = document.querySelector("div.cursor-outer")
 
-const motion = window.matchMedia("(prefers-reduced-motion: no-preference)")
-const large = window.matchMedia('(min-width: 600px)')
+const mq = window.matchMedia("(prefers-reduced-motion: no-preference) and (min-width: 600px)")
 
-if (motion.matches && large.matches) {
-    mainTag.style.position = 'fixed'
-    mainTag.style.left = '0px'
-    mainTag.style.top = '0px'
-    mainTag.style.width = '100%'
+const runScripts = function () {
+    if (mq.matches) {
+        mainTag.style.position = 'fixed'
+        mainTag.style.left = '0px'
+        mainTag.style.top = '0px'
+        mainTag.style.width = '100%'
 
-    let observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            console.log(entry.target)
-            if (entry.intersectionRatio > 0.25) { // число пересечения target элемента
-                entry.target.classList.add('in-view')
-            } else {
-                entry.target.classList.remove("in-view")
-            }
+        let observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                console.log(entry.target)
+                if (entry.intersectionRatio > 0.25) { // число пересечения target элемента
+                    entry.target.classList.add('in-view')
+                } else {
+                    entry.target.classList.remove("in-view")
+                }
+            })
+        }, {
+            threshold: [0, 0.25, 1]
         })
-    }, {
-        threshold: [0, 0.25, 1]
-    })
-    figcaptionTags.forEach(caption => {
-        observer.observe(caption);
-    })
+        figcaptionTags.forEach(caption => {
+            observer.observe(caption);
+        })
 
-    let currentScroll = 0
-    let aimScroll = 0
+        let currentScroll = 0
+        let aimScroll = 0
 
-    const changeScroll = function () {
-        bodyTag.style.height = mainTag.offsetHeight + 'px'
+        const changeScroll = function () {
+            bodyTag.style.height = mainTag.offsetHeight + 'px'
 
-        currentScroll = currentScroll + (aimScroll - currentScroll) * 0.05
-        // плавная прокрутка - smooth scroll
-        // плавность достигается частым вызовом функции
-        mainTag.style.top = (-1 * currentScroll) + "px"
-        requestAnimationFrame(changeScroll)
-    }
-    window.addEventListener("scroll", function () {
-        aimScroll = window.scrollY
+            currentScroll = currentScroll + (aimScroll - currentScroll) * 0.05
+            // плавная прокрутка - smooth scroll
+            // плавность достигается частым вызовом функции
+            mainTag.style.top = (-1 * currentScroll) + "px"
+            requestAnimationFrame(changeScroll)
+        }
+        window.addEventListener("scroll", function () {
+            aimScroll = window.scrollY
 
-    })
+        })
 
-    changeScroll()
+        changeScroll()
 
 //    **** CURSOR ****
-    let cursorCurrentX = 0
-    let cursorCurrentY = 0
-    let cursorOuterCurrentX = 0
-    let cursorOuterCurrentY = 0
+        let cursorCurrentX = 0
+        let cursorCurrentY = 0
+        let cursorOuterCurrentX = 0
+        let cursorOuterCurrentY = 0
 
-    let cursorAimX = 0
-    let cursorAimY = 0
+        let cursorAimX = 0
+        let cursorAimY = 0
 
-    const changeCursor = () => {
-        cursorCurrentX = cursorCurrentX + (cursorAimX - cursorCurrentX) * 0.1
-        cursorCurrentY = cursorCurrentY + (cursorAimY - cursorCurrentY) * 0.1
+        const changeCursor = () => {
+            cursorCurrentX = cursorCurrentX + (cursorAimX - cursorCurrentX) * 0.1
+            cursorCurrentY = cursorCurrentY + (cursorAimY - cursorCurrentY) * 0.1
 
-        cursorTag.style.top = cursorCurrentY + 'px'
-        cursorTag.style.left = cursorCurrentX + 'px'
+            cursorTag.style.top = cursorCurrentY + 'px'
+            cursorTag.style.left = cursorCurrentX + 'px'
 
-        cursorOuterCurrentX = cursorOuterCurrentX + (cursorAimX - cursorOuterCurrentX) * 0.05
-        cursorOuterCurrentY = cursorOuterCurrentY + (cursorAimY - cursorOuterCurrentY) * 0.05
+            cursorOuterCurrentX = cursorOuterCurrentX + (cursorAimX - cursorOuterCurrentX) * 0.05
+            cursorOuterCurrentY = cursorOuterCurrentY + (cursorAimY - cursorOuterCurrentY) * 0.05
 
-        cursorOuterTag.style.top = cursorOuterCurrentY + 'px'
-        cursorOuterTag.style.left = cursorOuterCurrentX + 'px'
+            cursorOuterTag.style.top = cursorOuterCurrentY + 'px'
+            cursorOuterTag.style.left = cursorOuterCurrentX + 'px'
 
-        requestAnimationFrame(changeCursor)
+            requestAnimationFrame(changeCursor)
+        }
+
+        document.addEventListener("mousemove", function (event) {
+            cursorAimX = event.pageX
+            cursorAimY = event.pageY
+        })
+        changeCursor()
     }
-
-    document.addEventListener("mousemove", function (event) {
-        cursorAimX = event.pageX
-        cursorAimY = event.pageY
-    })
-    changeCursor()
 }
-
 
 //    **** HOVER EFFECT ****
 const linksTag = document.querySelectorAll("nav a")
@@ -99,3 +99,5 @@ linksTag.forEach(link => {
         }, 300)
     })
 })
+
+runScripts()
